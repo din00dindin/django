d7ba7f1fc56b52ab14c7a51e138f02167b06730a
Your Name <you@example.com>
2026-03-04 18:16:35 +0500

    articles

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 class Articles(models.Model):
     title = models.CharField(max_length=255)
--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Articles
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_Articles_list = Articles.objects.all
+    template = loader.get_template("articles/index.html")
+    context = {"latest_Articles_list": latest_Articles_list}
+    return HttpResponse(template.render(context, request))
+
+def detail(request, Articles_id):
+    return HttpResponse("You're looking at Articles %s." % Articles_id)
